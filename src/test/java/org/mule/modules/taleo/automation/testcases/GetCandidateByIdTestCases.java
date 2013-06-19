30278ea568cfdebf7030a133e8261db0d19b93f6
Hernan Wittemoller <hernan.wittemoller@mulesoft.com>
2013-06-19 17:06:09 -0300

    Committing automation changes for CLDCONNECT-942

---
+++ src/test/java/org/mule/modules/taleo/automation/testcases/GetCandidateByIdTestCases.java
@@ -0,0 +1,98 @@
+/**
+ * Mule Taleo Cloud Connector
+ *
+ * Copyright (c) MuleSoft, Inc. All rights reserved. http://www.mulesoft.com
+ */
+
+package org.mule.modules.taleo.automation.testcases;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.mule.api.MuleEvent;
+import org.mule.api.processor.MessageProcessor;
+import org.mule.modules.taleo.model.CandidateBean;
+
+public class GetCandidateByIdTestCases extends TaleoTestParent {
+	
+	private Long expectedCandidateId;
+	private String expectedCandidateEmail;
+	
+	@Before
+	public void setUp() {
+		
+    	CandidateBean candidateBean = (CandidateBean) context.getBean("getCandidateByIdCandidateBean");
+    	expectedCandidateEmail = String.format("%s@email.com", UUID.randomUUID().toString().substring(0, 8));
+    	candidateBean.setEmail(expectedCandidateEmail);
+    	
+    	testObjects =  new HashMap<String,Object>();
+    	testObjects.put("candidateRef", candidateBean);
+    	
+		MessageProcessor flow = lookupFlowConstruct("create-candidate");
+    	
+		try {
+
+			MuleEvent response = flow.process(getTestEvent(testObjects));
+			expectedCandidateId = (Long) response.getMessage().getPayload();
+			testObjects.put("candidateId", expectedCandidateId);
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			fail();
+		}
+		
+	}	
+	
+	
+	@After
+	public void tearDown() {
+		
+		MessageProcessor flow = lookupFlowConstruct("delete-candidate");
+		
+		try {		
+			
+			if (testObjects.containsKey("candidateId")) {
+				
+				flow.process(getTestEvent(testObjects));
+				
+			}
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+				e.printStackTrace();
+				fail();
+		}
+		
+	}
+
+    @Category({SmokeTests.class, RegressionTests.class})
+	@Test
+	public void testGetCandidateById() {
+    	
+		MessageProcessor flow = lookupFlowConstruct("get-candidate-by-id");
+    	
+		try {
+
+			MuleEvent response = flow.process(getTestEvent(testObjects));
+			CandidateBean candidateBean = (CandidateBean) response.getMessage().getPayload();
+
+			assertEquals((Long) candidateBean.getId(), expectedCandidateId);
+			assertEquals(candidateBean.getEmail(), expectedCandidateEmail);
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			fail();
+		}
+     
+	}
+    
+}
