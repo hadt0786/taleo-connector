4d15afa6c8a78ec6fefa58216b2af85db07d5ad2
pablocabrera85 <pablo.cabrera@mulesoft.com>
2013-06-26 13:23:07 -0300

    Added some test cases for failling methods

--- src/test/java/org/mule/modules/taleo/TaleoConnectorTestDriver.java
+++ src/test/java/org/mule/modules/taleo/TaleoConnectorTestDriver.java
@@ -10,24 +10,62 @@
 
 package org.mule.modules.taleo;
 
+import static org.junit.Assert.assertNotNull;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.mule.api.ConnectionException;
 import org.mule.modules.taleo.client.TaleoException;
+import org.mule.modules.taleo.model.DepartmentBean;
+import org.mule.modules.taleo.model.RequisitionBean;
+import org.mule.modules.taleo.model.UserBean;
 
 public class TaleoConnectorTestDriver {
 
 	private static TaleoConnector connector;
 	@BeforeClass
-	public static void setup(){
+	public static void setup() throws ConnectionException{
 		connector=new TaleoConnector();
 		connector.setDispatcherUrl("https://tbe.taleo.net/MANAGER/dispatcher/servlet/rpcrouter");
-		connector.setCompanyCode(System.getenv("taleoCompanyCode"));
+		connector.setCompanyCode( System.getenv("taleo.companyCode"));
+		connector.connect( System.getenv("taleo.user"),  System.getenv("taleo.password"));
 	}
 	
 	@Test
-	public void getUrlTest() throws TaleoException, ConnectionException{
-		connector.connect(System.getenv("taleoUser"), System.getenv("taleoPassword"));
-		connector.disconnect();
+	public void getDepartment() throws TaleoException, ConnectionException{
+		DepartmentBean department=new DepartmentBean();
+		department.setDepartmentName("Foo4Department");
+		
+		long departmentId = connector.createDepartment(department);
+		DepartmentBean departmentRetrieved=connector.getDepartmentById(departmentId);
+		assertNotNull(departmentRetrieved);
+		connector.deleteDepartment(departmentRetrieved.getDepartmentId());
+		
+	}
+	
+	@Test
+	public void getUserTest() throws TaleoException, ConnectionException{
+		UserBean user=new UserBean();
+		user.setLastName("FooTest");
+		user.setRole("E");
+		user.setLoginName("dummyloginname");
+		user.setEmail("foo@foo.com.ar");
+		user.setStatus("Hired");
+		long userId=connector.createUser(user);
+		UserBean userRetrieved=connector.getUserById(userId);
+		assertNotNull(userRetrieved);
+		connector.deleteUser(userId);	
+	}
+	
+	@Test
+	public void getRequisitionTest() throws TaleoException, ConnectionException{
+		RequisitionBean req=new RequisitionBean();
+		req.setTitle("FooReqTemplate");
+		req.setStatus("Waiting for Approval");
+		req.setLocation("Headquarters");
+		long reqId=connector.createRequisitionTemplate(req);
+		RequisitionBean reqRetrieved=connector.getRequisitionById(reqId);
+		assertNotNull(reqRetrieved);
+		connector.deleteRequisition(reqId);
 	}
 }
