6e7282c19e046547879283724665e7bb30a3223d
Jose Sahad <jose.amenta@mulesoft.com>
2013-07-04 14:59:54 -0300

    Update license

--- src/test/java/org/mule/modules/taleo/automation/SmokeTestSuite.java
+++ src/test/java/org/mule/modules/taleo/automation/SmokeTestSuite.java
@@ -1,7 +1,9 @@
 /**
- * Mule Taleo Cloud Connector
- *
- * Copyright (c) MuleSoft, Inc. All rights reserved. http://www.mulesoft.com
+ * (c) 2003-2012 MuleSoft, Inc. This software is protected under international
+ * copyright law. All use of this software is subject to MuleSoft's Master
+ * Subscription Agreement (or other Terms of Service) separately entered
+ * into between you and MuleSoft. If such an agreement is not in
+ * place, you may not use the software.
  */
 
 package org.mule.modules.taleo.automation;
